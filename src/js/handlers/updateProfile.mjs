ebf041206882af9dd8be00060a61da3c190f14ac
Ingeburgern <56824264+Spekkhogger@users.noreply.github.com>
2023-05-19 20:30:12 +0200

    Add update profile

---
+++ src/js/handlers/updateProfile.mjs
@@ -0,0 +1,29 @@
+
+export async function updateProfileHandler(){
+    const updatePostForm = document.querySelector("#update-profile-form")
+
+    const url = new URL(location.href); 
+    const id = url.searchParams.get("id");
+
+    if (updatePostForm){
+        const oldPost = await getPost(id); 
+        console.log(oldPost);
+
+        updatePostForm.title.value = oldPost.title; 
+        updatePostForm.body.value = oldPost.body; 
+
+
+        updatePostForm["tags[]"].value = oldPost.tags; 
+        updatePostForm.media.value = oldPost.media; 
+
+        updatePostForm.addEventListener("submit", (event) =>{
+            event.preventDefault(); 
+            const form = event.target;
+            const formData = new FormData(form); 
+            const post = Object.fromEntries(formData.entries()); 
+            post.id = id;
+
+            updatePostFunc(post);
+        })
+    }
+}
