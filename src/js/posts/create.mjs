ba1dab564c08b729b32d2b24fe1638a92a4e03dc
Ingeburgern <56824264+Spekkhogger@users.noreply.github.com>
2023-05-19 13:45:53 +0200

    add templates

--- src/js/posts/create.mjs
+++ src/js/posts/create.mjs
@@ -2,6 +2,13 @@ import { baseURL } from "../constants.mjs";
 const postPath = "/posts";
 import { authFetch } from "./fetch.mjs";
 
+/**
+ * Creates a new post by sending a POST request to the server.
+ * @param {Object} newPost - The new post object to be created.
+ * @returns {Promise<Object>} A promise that resolves to the created post object.
+ * @throws {Error} If an error occurs during the creation process.
+ */
+
 export async function create(newPost) {
     const postURL = baseURL + postPath; 
 
